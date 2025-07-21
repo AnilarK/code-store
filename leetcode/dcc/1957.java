ce23edfd30c52169b154bbb5f716599fd29a0f2d
ABHAY SINGH <93884059+AnilarK@users.noreply.github.com>
2025-07-22 01:33:26 +0530

    Update 1957.java

---
+++ leetcode/dcc/1957.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String makeFancyString(String s) {
+        StringBuilder ans = new StringBuilder();
+        int n=s.length();
+        for(int i=0;i<n;){
+            char p = s.charAt(i);
+            int cnt = 0;
+            while(i<n && s.charAt(i)==p){
+                i+=1; cnt+=1;}
+            cnt=Math.min(cnt,2);
+            while(cnt>0){
+                cnt--;
+                ans.append(p); }
+        }
+        return ans.toString();
+    }
+}
