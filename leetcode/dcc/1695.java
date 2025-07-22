9fb66fcfa9c4db4223df2f888c9826a8f5151fa1
AnilarK <abhayanilark@gmail.com>
2025-07-23 00:48:14 +0530

    added 1695.java

---
+++ leetcode/dcc/1695.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximumUniqueSubarray(int[] a) {
+        int n = a.length , ans=0,x=0 ,i=0 ,s=0;
+        int[] m = new int[10001];
+        for(int j=0;j<n;j++){
+            if(m[a[j]]==0){s++;}
+            m[a[j]]++;
+            x+=a[j];
+            while( s < (j-i+1) ){
+                x-=a[i];
+                if(m[a[i]]==1){s--;}
+                m[a[i]]--; i++;}
+            ans=Math.max(ans,x);  }   
+        return ans;
+    }
+}
