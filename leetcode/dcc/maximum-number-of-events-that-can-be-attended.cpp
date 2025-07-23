1a96798917e4b3b160a6571ac9c03a4992277635
ABHAY SINGH <93884059+AnilarK@users.noreply.github.com>
2025-07-23 12:56:56 +0530

    Add solution for : maximum-number-of-events-that-can-be-attended

---
+++ leetcode/dcc/maximum-number-of-events-that-can-be-attended.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/maximum-number-of-events-that-can-be-attended/
+
+class Solution {
+public:
+    int maxEvents(vector<vector<int>>& a) {
+        int n=a.size(),ans=0 ;
+        vector<vector<int>>x(1e5 + 10);
+        for(int i=0;i<n;i++){
+            x[a[i][0]-1].push_back(a[i][1]-1);}
+        map<int,int>m;
+        for(int i=0;i<=1e5;i++){
+            for(int j=0;j<x[i].size();j++){
+                m[x[i][j]]++;}
+            while(m.size()!=0 && m.begin()->first < i){
+                m.erase(m.begin()->first);}
+            if(m.size()!=0 && (m.begin())->first >=i){
+                ans++;
+                int it=(m.begin())->first;
+                m[it]--;
+                if(m[it]==0){m.erase(it);}
+            }
+        }
+        return ans;
+        
+    }
+};
