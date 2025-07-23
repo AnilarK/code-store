c2751f498575b510d70f1a96e701a2913f27023e
ABHAY SINGH <93884059+AnilarK@users.noreply.github.com>
2025-07-23 12:56:45 +0530

    Add solution for : longest-harmonious-subsequence

---
+++ leetcode/dcc/longest-harmonious-subsequence.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/longest-harmonious-subsequence/
+
+class Solution {
+public:
+    int findLHS(vector<int>& a) {
+        int n=a.size(),ans=0;
+        unordered_map<int,int>m;
+        for(int i=0;i<n;i++){m[a[i]]++;}
+        for(auto it=m.begin();it!=m.end();it++){
+            int p=it->first;
+            if(m.find(p+1)==m.end()) continue;
+            ans=max(ans,m[p]+m[p+1]);}
+        return ans;
+    }
+};
