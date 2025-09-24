0f64779551d8875f82c9e5d25ff2bb25676edbd7
AnilarK <abhayanilark@gmail.com>
2025-09-24 17:16:22 +0530

    switch_case

---
+++ striver-sheet/learn_the_basics/switch_case.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class switch_case {
+
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number between 1 and 7 included: ");
+        int day = sc.nextInt();
+
+        switch(day){
+            case 1:
+                System.out.print("Monday");
+                break;
+            case 2:
+                System.out.print("Tuesday");
+                break;
+            case 3:
+                System.out.print("Wednesday");
+                break;
+            case 4:
+                System.out.print("Thursday");
+                break;
+            case 5:
+                System.out.print("Friday");
+                break;
+            case 6:
+                System.out.print("Saturday");
+                break;
+            case 7:
+                System.out.print("Sunday");
+                break;
+            default:
+                System.out.print("Invalid number");
+        }
+
+
+    }
+
+}
