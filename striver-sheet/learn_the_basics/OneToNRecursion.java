8d073b86042670788a6f87f6e2ff216ae6037c3a
AnilarK <abhayanilark@gmail.com>
2025-09-27 13:50:50 +0530

    Recusrion 1->N and N->1

--- striver-sheet/learn_the_basics/OneToNRecursion.java
+++ striver-sheet/learn_the_basics/OneToNRecursion.java
@@ -8,12 +8,23 @@ class OneToNRecursion {
         System.out.println("the number is : " + n);
     }
 
+    private static void printNTo1(int n){
+        if(n<=0) return;
+        System.out.println("the number is : " + n);
+        printNTo1(n-1);
+    }
+
     public static void main(String args[]){
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number : ");
         int n = sc.nextInt();
+
+        System.out.println("Output 1 To N : ");
         print1ToN(n);
+
+        System.out.println("Output N To 1 : ");
+        printNTo1(n);
     }
 
 }
