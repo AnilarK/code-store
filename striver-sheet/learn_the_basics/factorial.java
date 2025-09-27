9faa3b7099b4c834b93e94c9b8ebd4ca375b73f6
AnilarK <abhayanilark@gmail.com>
2025-09-27 14:17:20 +0530

    factorial

---
+++ striver-sheet/learn_the_basics/factorial.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class factorial{
+
+    private static int fact(int n){
+        if(n<=0) return 1;
+        return n*fact(n-1);
+    }
+
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number : ");
+
+        int n =  sc.nextInt();
+
+        System.out.println("Factorial of the number is : " + fact(n) );
+
+    }
+
+}
