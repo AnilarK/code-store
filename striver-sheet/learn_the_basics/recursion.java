dc40d9865c67b4b9d3bfc5bb9e4fa55ff63575bd
AnilarK <abhayanilark@gmail.com>
2025-09-27 13:40:40 +0530

    recusrion

---
+++ striver-sheet/learn_the_basics/recursion.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class recursion {
+
+    private static void printN(String name, int n){
+        if(n==0) return;
+
+        System.out.println("the String is : " + name);
+        printN(name,n-1);
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        System.out.println("Enter any string");
+        String name = sc.next();
+
+        printN(name,n);
+
+    }
+
+}
